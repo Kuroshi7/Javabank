9e4d39a71b0e3c881a0fc9b373b7ffd8c642c2df
Gabriel Bunhak <bunhakgabriel@gmail.com>
2024-04-02 05:01:38 -0300

    commit de teste

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,5 +13,6 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        System.out.println("Testando commit");
     }
 }
