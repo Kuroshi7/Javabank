348412c2ec97093ca2f8ad355ec83516cb5badc2
Victor Soffi <victorsoffi@gmail.com>
2024-04-03 01:28:33 -0300

    ambiente pomxml + web

---
+++ src/main/java/org/example/BankApp.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApp {
+    public static void main(String[]args){
+        SpringApplication.run(BankApp.class,args);
+        System.out.println("Alo Mundo Spring Boot");
+    }
+}
