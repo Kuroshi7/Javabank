d6abfdd7dd8204485999ed1c23409111275f8019
Victor Soffi <victorsoffi@gmail.com>
2024-04-09 18:07:02 -0300

    cadastro att

--- BackEnd/src/main/java/org/example/firebase/Customer.java
+++ BackEnd/src/main/java/org/example/firebase/Customer.java
@@ -8,7 +8,28 @@ public class Customer {
     private String name;
     private String email;
     private String id;
+    private String cpf;
+    private String idade;
+    private String senha;
     
+    public String getsenha() {
+        return senha;
+    }
+    public void setsenha(String senha) {
+        this.senha = senha;
+    }
+    public String getcpf() {
+        return cpf;
+    }
+    public void setcpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getIdade() {
+        return idade;
+    }
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
     public String getName() {
         return name;
     }
