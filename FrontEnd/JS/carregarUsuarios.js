025295d95bfc66fc1f51847f277e4348e038454e
Victor Soffi <victorsoffi@gmail.com>
2024-04-10 11:39:49 -0300

    filtragem de usuario na lista

--- FrontEnd/JS/carregarUsuarios.js
+++ FrontEnd/JS/carregarUsuarios.js
@@ -3,16 +3,29 @@ function carregarUsuarios() {
         .then(response => response.json())
         .then(data => {
             const usuarios = document.getElementById('usuarios');
-            usuarios.innerHTML = ''; 
+            usuarios.innerHTML = '';
             data.list.forEach(usuario => {
                 const divUsuario = document.createElement('div');
                 divUsuario.classList.add('usuario');
                 divUsuario.innerHTML = `Nome: <span class="nome">${usuario.name}</span><br>E-mail: <span class="email">${usuario.email}</span><br> CPF: <span class="cpf">${usuario.cpf}</span>`;
                 usuarios.appendChild(divUsuario);
             });
+
+            
+            const filtroInput = document.getElementById('filtroInput');
+            filtroInput.addEventListener('input', () => {
+                const filtro = filtroInput.value.toLowerCase();
+                const usuariosFiltrados = data.list.filter(usuario => usuario.name.toLowerCase().startsWith(filtro));
+                usuarios.innerHTML = '';
+                usuariosFiltrados.forEach(usuario => {
+                    const divUsuario = document.createElement('div');
+                    divUsuario.classList.add('usuario');
+                    divUsuario.innerHTML = `Nome: <span class="nome">${usuario.name}</span><br>E-mail: <span class="email">${usuario.email}</span><br> CPF: <span class="cpf">${usuario.cpf}</span>`;
+                    usuarios.appendChild(divUsuario);
+                });
+            });
         })
         .catch(error => console.error('Erro:', error));
 }
-
 
 document.addEventListener('DOMContentLoaded', carregarUsuarios);
