c083c8552ec1a5e585b28b5c4edf22f046adc660
Victor Soffi <victorsoffi@gmail.com>
2024-05-21 13:30:48 -0300

    simbolo $

--- FrontEnd/JS/usuario.js
+++ FrontEnd/JS/usuario.js
@@ -6,7 +6,7 @@ const key = urlParams.get('key')
 const renderUser = (usuario) => {
     const ul = document.getElementById('dados')
     document.querySelector('.conta').innerText += usuario.name + " " + usuario.cpf
-    document.querySelector('.saldo').innerText += " " + usuario.contaCorrente.saldo;
+    document.querySelector('.saldo').innerText += " " + usuario.contaCorrente.saldo + " R$";
     ul.innerHTML = `
         <li>Email: ${usuario.email}</li>
         <li>CPF: ${usuario.cpf}</li>
